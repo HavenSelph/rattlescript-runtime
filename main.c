012293a94b945a5f5d9dbd0848b1ec8519325a01
HavenSelph <havenselph@gmail.com>
2023-04-18 00:01:10 -0500

    Testing

--- main.c
+++ main.c
@@ -6,27 +6,80 @@
 #include "Types/hash_map.h"
 #include "Types/linked_list.h"
 
+typedef struct Queue {
+    LinkedList *ll;
+    LLNode *front;
+    LLNode *rear;
+    int size;
+} Queue;
+
+
+Queue *queue_new() {
+    Queue *queue = allocate(1, sizeof(Queue));
+    queue->ll = ll_new();
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+    return queue;
+}
+
+void queue_push(Queue *queue, Value *val) {
+    // F v v v R
+    //         ^x
+    // F v v v x R
+
+    ll_push(queue->ll, val);
+    if (queue->size++ == 0) {
+        queue->front = queue->ll->start;
+    }
+    queue->rear = queue->ll->end;
+}
+
+
+Value *queue_pop(Queue *queue) {
+    Value *val = NULL;
+    if (queue->size == 0) {
+        printf("Queue is empty\n");
+        exit(1);
+    } else {
+        val = queue->front->val;
+        if (queue->size == 1) {
+            queue->front = NULL;
+            queue->rear = NULL;
+        } else {
+            queue->front = queue->front->next;
+        }
+    }
+    queue->size--;
+    return val;
+}
+
+void queue_print(Queue *queue) {
+    // print from head to tail
+    LLNode *cur = queue->front;
+    printf("Queue: ");
+    for (int i = 0; i < queue->size; ++i) {
+        value_print(cur->val);
+        if (i!=queue->size) printf("->");
+        cur = cur->next;
+    }
+}
+
 
 int main() {
-    // Value *val = value_new_list(list_new(10));
-    // Value *val2 = value_new_list(list_new(10));
-    // for (int i = 0; i < 10; i++) {
-    //     list_push(val->as_list, value_new_int(i));
-    //     list_push(val2->as_list, value_new_int(i));
-    // }
-    // list_push(val->as_list, value_ref(val2));
-    // value_free(val2);
-    // value_print(val);
-    // value_free(val);
+    LinkedList *ll = ll_new();
+    ll_push(ll, value_new_int(1));
+    ll_push(ll, value_new_int(2));
+    ll_push_front(ll, value_new_int(3));
 
-    Value *val = value_new_int(10);
-    Value *val2 = value_new_int(10);
-    Value *val3 = value_equals(ref(val), ref(val2));
-    if (value_as_bool_c(value_subtract(val,val2))) {
-        printf("True");
-    } else {
-        printf("False");
+    Value *list = value_new_list(list_new(10));
+    for (int i = 0; i < 100; ++i) {
+        list_push(list->as_list, value_new_int(i));
     }
-    value_print(val3);
-    value_free(val3);
+    ll_push_front(ll, ref(list));
+    ll_print(ll);
+
+    Value *linked = value_new_linked_list(ll);
+    value_free(linked);
+    unref(list);
 }
